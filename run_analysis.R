8b5cc60a06997e383fa0be1b787b1cafe15b4714
Tobias <Tobias1988@users.noreply.github.com>
2014-06-18 17:05:14 +0200

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,17 @@
+features<-read.table("features.txt")
+features<-features$V2
+xtest<- read.table("X_test.txt")
+xtrain<-read.table("x_train.txt")
+combinex<- rbind(xtest, xtrain)
+colnames(combinex)<-features
+testmean <- combinex[,grep("mean", names(combinex))]
+testMean <- combinex[,grep("Mean", names(combinex))]
+testMm <- cbind(testMean, testmean)
+teststd <- combinex[,grep("std", names(combinex))]
+MeansAndStd<-cbind(testMm, teststd)
+Tdata<-MeansAndStd
+VariableAvg<- colMeans(MeansAndStd)
+ObservationAvg<-rowMeans(MeansAndStd)
+capture.output(Tdata, file="TidyData", append=TRUE)
+capture.output(ObservatiionAvg, file="ObservationAvg", append=TRUE)
+capture.output(VariableAvg, file="VariableAvg", append=TRUE)
